60fc41afd269ba60699752cea33043e4fe3c6fa1
IanRDavies <ian_davies_@hotmail.co.uk>
2016-10-06 16:01:07 +0100

    Added depth to pages

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,18 @@ function RoutesConfig($stateProvider, $urlRouteProvider){
 		url: "/",
 		templateUrl: "src/templates/home.template.html",
 		controller: "WelcomeController as main"
+	})
+	.state("about", {
+		url: "/about",
+		templateUrl: "src/templates/about.template.html"
+	})
+	.state("ourServices", {
+		url: "/our-services",
+		templateUrl: "src/templates/services.template.html"
+	})
+	.state("contactUs", {
+		url: "/contact-us",
+		templateUrl: "src/templates/contact.template.html"
 	});
 };
 
